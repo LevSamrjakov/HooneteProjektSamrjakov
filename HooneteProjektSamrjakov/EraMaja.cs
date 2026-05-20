ef9c3b112aaf077f97534d92136673e27aefaa2d
opilane <opilane@HP-CZC2349HTY>
2026-05-20 15:45:05 +0300

    + Alamklassid ja Polümorfism

---
+++ HooneteProjektSamrjakov/EraMaja.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HooneteProjektSamrjakov
+{
+    public class EraMaja : Hoone
+    {
+        public bool OnElektriautoLaadija;
+
+        public EraMaja(string aadress, int pindala, Uks majaUks, bool laadija) : base(aadress, pindala, majaUks)
+        {
+            OnElektriautoLaadija = laadija;
+        }
+
+        public override double Ülalpidamiskulu()
+        {
+            double kulu = Pindala * 12.5;
+            if (OnElektriautoLaadija)
+            {
+                kulu += 600;
+            }
+            return kulu;
+        }
+    }
+}
---
+++ HooneteProjektSamrjakov/KorteriteMaja.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HooneteProjektSamrjakov
+{
+    public class KorteriteMaja : Hoone
+    {
+        public int KorteriteArv { get; set; }
+
+        public KorteriteMaja(string aadress, int pindala, Uks majaUks, int korteriteArv) : base(aadress, pindala, majaUks)
+        {
+            KorteriteArv = korteriteArv;
+        }
+
+        public override double Ülalpidamiskulu()
+        {
+            double kulu = (Pindala * 8.0) + (KorteriteArv * 150);
+            return kulu;
+        }
+    }
+}
